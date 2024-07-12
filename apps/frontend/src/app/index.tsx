177f56d0406b4f734e6b6926db99c83687786e27
morhaham2 <morh@codeoasis.com>
2024-07-12 11:27:58 +0300

    shadcn

---
+++ apps/frontend/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
